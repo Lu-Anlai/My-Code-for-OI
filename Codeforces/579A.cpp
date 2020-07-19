51159c84db3644957a1a1fa3deea07c7abf7b6b0
Lu-Anlai <Lu@Lu-Anlai.com>
2020-07-19 22:58:49 +0800

    2020-07-19-22:58

--- Codeforces/579A.cpp
+++ Codeforces/579A.cpp
@@ -2,16 +2,14 @@
 
 int x;
 
-int main(void)
-{
-    register int cnt = 0;
-    scanf("%d", &x);
-    while (x)
-    {
-        if (x & 1)
-            ++cnt;
-        x >>= 1;
-    }
-    printf("%d\n", cnt);
-    return 0;
+int main(void){
+	register int cnt=0;
+	scanf("%d",&x);
+	while(x){
+		if(x&1)
+			++cnt;
+		x>>=1;
+	}
+	printf("%d\n",cnt);
+	return 0;
 }
--- Codeforces/982A.cpp
+++ Codeforces/982A.cpp
@@ -6,16 +6,16 @@ int n;
 
 int main(void)
 {
-    register int i;
-    scanf("%d%s", &n, str + 1);
-    str[0] = str[n + 1] = '0';
-    for (i = 1; i <= n; ++i)
-        if (str[i] == '0' && !(str[i - 1] == '1' || str[i + 1] == '1'))
-            return puts("No"), 0;
-        else if (str[i] == '1' && (str[i - 1] == '1' || str[i + 1] == '1'))
-            return puts("No"), 0;
-        else
-            continue;
-    puts("Yes");
-    return 0;
+	register int i;
+	scanf("%d%s", &n, str + 1);
+	str[0] = str[n + 1] = '0';
+	for (i = 1; i <= n; ++i)
+		if (str[i] == '0' && !(str[i - 1] == '1' || str[i + 1] == '1'))
+			return puts("No"), 0;
+		else if (str[i] == '1' && (str[i - 1] == '1' || str[i + 1] == '1'))
+			return puts("No"), 0;
+		else
+			continue;
+	puts("Yes");
+	return 0;
 }
