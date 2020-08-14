69f3b59474a29943ea2b5adee1441e4bcd96a3d3
Lu-Anlai <Lu@Lu-Anlai.com>
2020-08-14 11:46:45 +0800

    2020-08-14-11:46

---
+++ Luogu/P4770.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define reg register
+typedef long long ll;
+
+const int MAXS=5e5+5;
+const int MAXT=1e6+5;
+
+char S[MAXS];
+int Q;
+char T[MAXT];
+
+int main(void){
+	scanf("%s",S+1);
+	scanf("%d",&Q);
+	while(Q--){
+		static int l,r;
+		scanf("%s%d%d",T+1,&l,&r);
+		
+	}
+	return 0;
+}
