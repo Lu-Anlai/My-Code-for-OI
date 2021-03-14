967a524560d068b2e50beaa609c161bc6ce11166
Lu-Anlai <Lu@Lu-Anlai.com>
2021-03-14 15:54:20 +0800

    2021-03-14-15:54

---
+++ tmp.c
@@ -0,0 +1,132 @@
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
+#include<stdio.h>
+/*
+std:standard
+io:input&output
+h:header
+*/
+
+bgcd
+
+
+sadasdasdasdasdada  asdas
